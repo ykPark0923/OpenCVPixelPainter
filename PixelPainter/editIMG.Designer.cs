64ece6b5f5113f1d5ee6ed25dc5d0196154915b6
ykPark0923 <puklove22@naver.com>
2025-01-22 22:48:57 +0900

    Initial commit

---
+++ PixelPainter/editIMG.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PixelPainter
+{
+    partial class editIMG
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openBTN = new System.Windows.Forms.Button();
+            this.saveBTN = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.OpcomboBox = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 294);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(336, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 294);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openBTN
+            // 
+            this.openBTN.Location = new System.Drawing.Point(216, 332);
+            this.openBTN.Name = "openBTN";
+            this.openBTN.Size = new System.Drawing.Size(75, 31);
+            this.openBTN.TabIndex = 2;
+            this.openBTN.Text = "열기";
+            this.openBTN.UseVisualStyleBackColor = true;
+            this.openBTN.Click += new System.EventHandler(this.openBTN_Click);
+            // 
+            // saveBTN
+            // 
+            this.saveBTN.Location = new System.Drawing.Point(550, 332);
+            this.saveBTN.Name = "saveBTN";
+            this.saveBTN.Size = new System.Drawing.Size(75, 31);
+            this.saveBTN.TabIndex = 3;
+            this.saveBTN.Text = "저장";
+            this.saveBTN.UseVisualStyleBackColor = true;
+            this.saveBTN.Click += new System.EventHandler(this.saveBTN_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.OpcomboBox);
+            this.groupBox1.Location = new System.Drawing.Point(642, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 294);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "group";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "연산값";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 93);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(189, 26);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // OpcomboBox
+            // 
+            this.OpcomboBox.FormattingEnabled = true;
+            this.OpcomboBox.Items.AddRange(new object[] {
+            "OpAdd",
+            "OpSubtract",
+            "OpMultiply",
+            "OpDivide",
+            "OpMax",
+            "OpMin",
+            "OpAbs",
+            "OpAbDiff"});
+            this.OpcomboBox.Location = new System.Drawing.Point(12, 49);
+            this.OpcomboBox.Name = "OpcomboBox";
+            this.OpcomboBox.Size = new System.Drawing.Size(189, 26);
+            this.OpcomboBox.TabIndex = 0;
+            this.OpcomboBox.SelectedIndexChanged += new System.EventHandler(this.OpcomboBox_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // editIMG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.saveBTN);
+            this.Controls.Add(this.openBTN);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "editIMG";
+            this.Text = "editIMG";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button openBTN;
+        private System.Windows.Forms.Button saveBTN;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox OpcomboBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ PixelPainter/editIMG.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace PixelPainter
+{   
+    
+    enum ImageOperation
+    {
+        OpAdd = 0,
+        OpSubtract,
+        OpMultiply,
+        OpDivide,
+        OpMax,
+        OpMin,
+        OpAbs,
+        OpAbDiff
+    }
+
+
+    public partial class editIMG : Form
+    {        
+        public editIMG()
+        {
+            InitializeComponent();
+        }
+
+        private void openBTN_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {  //DialogResult는 windows forms 라이브러리에서 제공하는 열거형.
+                //대화상자(Dialog)에서 반환되는 값 표현
+                pictureBox1.Load(openFileDialog1.FileName);
+
+                Mat src1 = Cv2.ImRead(openFileDialog1.FileName);
+                //pictureBox1.Image = BitmapConverter.ToBitmap(src1);
+
+            }
+        }
+
+        private void saveBTN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpcomboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Mat src1 = Cv2.ImRead(openFileDialog1.FileName);
+            Mat src2 = new Mat(src1.Size(), MatType.CV_8UC3, new Scalar(0, 0, 30));
+
+            Mat dst = new Mat();
+
+            ImageOperation selType = ImageOperation.OpAbDiff;
+
+            switch (selType)
+            {
+                case ImageOperation.OpAdd:
+                    Cv2.Add(src1, src2, dst);
+                    break;
+                case ImageOperation.OpSubtract:
+                    Cv2.Subtract(src1, src2, dst);
+                    break;
+                case ImageOperation.OpMultiply:
+                    Cv2.Multiply(src1, src2, dst);
+                    break;
+                case ImageOperation.OpDivide:
+                    Cv2.Divide(src1, src2, dst);
+                    break;
+                case ImageOperation.OpMax:
+                    Cv2.Max(src1, src2, dst);
+                    break;
+                case ImageOperation.OpMin:
+                    Cv2.Min(src1, src2, dst);
+                    break;
+                case ImageOperation.OpAbs:
+                    Cv2.Multiply(src1, src2, dst);
+                    Cv2.Abs(dst);
+                    break;
+                case ImageOperation.OpAbDiff:
+                    Mat matMul = new Mat();
+                    Cv2.Multiply(src1, src2, matMul);
+                    Cv2.Absdiff(src1, matMul, dst);
+                    break;
+            }
+
+            string strOperation = selType.ToString();
+
+            Cv2.ImShow(strOperation, dst);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
